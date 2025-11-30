439edc792e431024fc5516dba6c5b788339b63f1
Devendra Singh <198864641+DevendraRajoriya@users.noreply.github.com>
2025-11-30 11:02:33 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,13 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://randompokemon.co/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://www.randompokemon.co/sitemap.xml",
+    host: "https://www.randompokemon.co",
   };
 }
